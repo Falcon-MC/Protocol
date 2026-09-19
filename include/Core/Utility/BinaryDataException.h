7361d1f03d57288c4b792afa5464aa68a6c19dc1
NaySurGithub <96527285+NaySurGithub@users.noreply.github.com>
2026-09-19 22:37:27 +0200

    refactor: use the nbt library

--- include/Core/Utility/BinaryDataException.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdexcept>
-#include <string>
-
-class BinaryDataException : public std::runtime_error {
-public:
-    explicit BinaryDataException(const std::string &message) : std::runtime_error(message) {}
-};
