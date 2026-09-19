413794248bcc2175c536663ef83a0d0f43554890
NaySurGithub <96527285+NaySurGithub@users.noreply.github.com>
2026-09-19 11:39:58 +0200

    refactor: centralize pi constants in MathConstants

---
+++ include/Core/Math/MathConstants.h
@@ -0,0 +1,10 @@
+#pragma once
+
+namespace MathConstants {
+    inline constexpr double PI = 3.14159265358979323846;
+    inline constexpr double DEGREES_TO_RADIANS = PI / 180.0;
+
+    inline constexpr float PI_F = (float) PI;
+    inline constexpr float TWO_PI_F = (float) (2.0 * PI);
+    inline constexpr float DEGREES_TO_RADIANS_F = PI_F / 180.0f;
+}
